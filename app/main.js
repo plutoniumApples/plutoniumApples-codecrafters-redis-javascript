9f08b36b925558304305f35e8ffe3fe1e39213ba
Elko Lemiso <elko.d@breachlock.com>
2022-11-15 17:31:29 +0100

    pass 1st stage

--- app/main.js
+++ app/main.js
@@ -3,9 +3,10 @@ const net = require("net");
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
-
 const server = net.createServer((connection) => {
-  // Handle connection
+  connection.on("data", () => {
+    connection.write("+PONG\r\n");
+  });
 });
 
 server.listen(6379, "127.0.0.1");
